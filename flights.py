b892ada91851eca25d0bca1a6e6afb17555699e3
Ashutosh Jha <aj97389@gmail.com>
2021-06-05 14:28:22 +0530

    Added filter for only non-stop flights

--- flights.py
+++ flights.py
@@ -13,22 +13,40 @@ from webdriver_manager.chrome import ChromeDriverManager
 OPTS = webdriver.ChromeOptions()
 OPTS.headless = False
 ATTRIBUTES = ['SNo.', 'Flight Name', 'Flight No.', 'Source','Start Time', 'Start Date', 'Source Name', 'Destination', 'End Time', 'End Date', 'Destination Name', 'Total Time', 'Flight Type', 'Price']
+MAX_NO_OF_TRIES = 5
+FOUND_DATA = False
 
 class Scraper:
     def __init__(self, source, dest, startDate):
-        self.source = source
+        self.source = source    
         self.dest = dest
         self.startDate = startDate        
         self.search_url="https://www.ixigo.com/search/result/flight?from="+ self.source + "&to=" + self.dest + "&date="+self.startDate+"&returnDate=&adults=2&children=0&infants=0&class=e&source=Search%20Form"
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=OPTS)
         self.data = {}
+        self.findClassTry = 0
+
+
+    def waitForElementToLoad(self, className):
+        try:
+            self.driver.find_element_by_class_name(className)
+            self.findClassTry = 0
+            return
+        except:
+            self.findClassTry += 1
+            if(self.findClassTry > MAX_NO_OF_TRIES):
+                return
+            else:
+                print("Waiting for the webpage..")
+                time.sleep(2)
+            self.waitForElementToLoad(className)
+            
 
     def searchByUrl(self, fileName):
         self.driver.get(self.search_url)
-        time.sleep(5)
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         ## Get number of pages
         try:
+            self.waitForElementToLoad('page-num')
             pagesFound = (self.driver.find_elements_by_class_name("page-num"))
         except:
             pagesFound = []
@@ -42,12 +60,12 @@ class Scraper:
                     pass
                 else:
                     page.click()
-                    time.sleep(3)                
                 index, flightData  = (self.searchForFlights(index, flightData))                    
         ## Writing data extracted into csv
         self.writeIntoCsv(flightData, fileName)
 
     def searchForFlights(self, index, flightData):
+        self.waitForElementToLoad('c-flight-listing-row-v2')
         flightList = self.driver.find_elements_by_class_name("c-flight-listing-row-v2")
         for flight in flightList:  
             airlineInfo = flight.find_element_by_class_name("airline-info")
@@ -65,17 +83,22 @@ class Scraper:
             totalTime, flightType = journeyInfo.text.split("\n")
             price = flight.find_element_by_class_name("c-price-display").text
             
-            flightData.append([index, flightName, flightNo, sourceCode, startTime, startDate, sourceName, destCode, endTime, endDate, destName, totalTime, flightType, price])
-            index+=1
+            if flightType.lower() == 'non-stop':
+                flightData.append([index, flightName, flightNo, sourceCode, startTime, startDate, sourceName, destCode, endTime, endDate, destName, totalTime, flightType, price])
+                index+=1
         return index, flightData
 
     def writeIntoCsv(self, flightData, fileName):
+        if len(flightData) ==  0:
+            print("Couldn't find any data.")
+            return
         fileName+=".csv"
         with open(fileName, 'w') as file:
             writer = csv.writer(file)
             writer.writerow(ATTRIBUTES)
             for row in flightData:
                 writer.writerow(row)
+            print("Data saved in " + fileName)
 
 while(True):
     try:
@@ -90,13 +113,9 @@ while(True):
         startDate = startDate.replace("/", "")
         a = Scraper(sourceCode, destCode, startDate)
         a.searchByUrl(fileName)
-        print('\n============================================================')
-        print('DATA SAVED IN ' + fileName + ".csv")
-        print('============================================================\n')
+
     except Exception as e:
         print(e)
-        print('\n============================================================')
         print("Try Again")
-        print('============================================================\n')
 
     
