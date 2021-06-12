04084a3d7825b19b0199fdff942120e68211d70f
Ashutosh Jha <aj97389@gmail.com>
2021-06-12 20:33:00 +0530

    Replaced calenderByPin API by findByPin

--- cowin.py
+++ cowin.py
@@ -1,8 +1,7 @@
 
-from typing_extensions import Final
 import requests
 from datetime import datetime,timedelta
-import json
+import time
 TOTAL_DAYS = 7
 MIN_AGE_LIMIT = 45
 PIN = ["562157", "560063"]
@@ -13,21 +12,20 @@ for i in range(TOTAL_DAYS):
 
 for i in days_total:
     final_dates.append(i.strftime("%d%m%y"))
-
+i = 0
 for pin in PIN:
     try:
         for date in final_dates:
-            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pin,date)
+            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(pin,date)
             result=requests.get(URL)
             json_result=result.json()
-            if json_result["centers"]:
-                for center_idx, center in enumerate(json_result["centers"]):
-                    for sess_idx, session in enumerate(center["sessions"]):
-                        if session["min_age_limit"] == MIN_AGE_LIMIT:
-                            if session["available_capacity"]>0:
-                                ("Date Available: " +  date)
-                                print(str(center_idx+1) + ". " + center["name"] + ", " + str(center["center_id"]) )
-                                print("Dose1:" + str(session["available_capacity_dose1"])) 
-                                print("Dose2:" + str(session["available_capacity_dose2"]))
+            if json_result["sessions"]:
+                for sess_idx, session in enumerate(json_result["sessions"]):
+                    if session["min_age_limit"] == MIN_AGE_LIMIT:
+                        if session["available_capacity"]>0:
+                            ("Date Available: " +  date)
+                            print(str(sess_idx+1) + ". " + session["name"] + ", " + str(session["center_id"]) )
+                            print("Dose1:" + str(session["available_capacity_dose1"])) 
+                            print("Dose2:" + str(session["available_capacity_dose2"]))
     except Exception as e:
         print(e)
